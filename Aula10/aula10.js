8be660a553c245410cc0c3f4629b7da86577614c
gui lopes <guilopestomaz@gmail.com>
2024-10-24 19:16:04 -0300

    aula 11
    continuação

---
+++ Aula10/aula10.js
@@ -0,0 +1,8 @@
+//ACESSO AO ÍNDICE DO ELEMENTO ARRAY
+const numeros = [10, 20, 30, 40, 50,]; // Pesquisar pelos índices 1 e 3 
+
+const valorNoIndice1 = numeros[1];
+const valorNoIndice3 = numeros[3];
+
+console.log(`Valor no índice 1: ${valorNoIndice1}`);
+//saída: 20 console.log(`Valor no índice 3: ${valorNoIndice3}`); // saida: 40
